b328804d3e12912bfaec8e1aa3fb4bd3b678038e
nathalieraffray <nathalie@3dverse.com>
2020-10-14 17:32:44 -0400

    Created Visual Studio project Linked soloud.lib

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "soloud/soloud.h"
+#include "soloud/soloud_wav.h"
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+
+    SoLoud::Soloud gSoloud; // SoLoud engine
+    SoLoud::Wav gWave;      // One wave file
+}
